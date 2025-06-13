8ef792539fb99aef4f718b8a456a034cde6f80cc
Maximus Champion <maximuschampion2002@gmail.com>
2025-06-13 15:26:53 +0300

    Registered Library in the Django admin panel

--- Library/admin.py
+++ Library/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book, Payment
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "Author", "Cover", "Inventory", "Daily_fee")
+    search_fields = ("title", "Author")
+    list_filter = ("Cover",)
+    ordering = ("title",)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "type", "borrowing_id", "money_to_pay", "session_id")
+    search_fields = ("session_id",)
+    list_filter = ("status", "type")
+    ordering = ("-id",)
